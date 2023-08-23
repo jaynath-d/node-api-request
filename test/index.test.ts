f5184057840f079e0aeb412aab8f4c5b7efaeb68
Jaynath Ray <126139404+jaynath-d@users.noreply.github.com>
2023-08-23 21:36:15 +0530

    Update index.test.ts

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,26 +2,23 @@ import NodeApiRequest, { ApiResponse } from '../src/index';
 
 describe('NodeApiRequest', () => {
 
-    // Tests that the sendRequest method successfully sends a GET request
-    it('should successfully send a GET request', () => {
-      // Mock the request function
+    // Tests that the class handles errors in the request properly
+    it('should handle errors in the request properly', () => {
+      // Mock the request function to simulate an error
       const mockRequest = jest.fn((options, callback) => {
-        callback(null, { statusCode: 200 }, { data: "response body" });
+        callback(new Error("Request error"), null, null);
       });
 
       // Replace the original request function with the mock
       jest.mock("request", () => mockRequest);
 
-      // Call the sendRequest method with GET options
-      const options = {
-        url: "http://example.com",
-        method: "GET",
-      };
-      return NodeApiRequest.sendRequest(options).then((response) => {
+      // Call the sendRequest method with options
+      const options:any = { url: "https://example.com", method: "GET" };
+      return NodeApiRequest.sendRequest(options).catch((error) => {
         // Assert that the mock request function was called with the correct options
         expect(mockRequest).toHaveBeenCalledWith(
           {
-            url: "http://example.com",
+            url: "https://example.com",
             method: "GET",
             headers: undefined,
             body: undefined,
@@ -30,9 +27,8 @@ describe('NodeApiRequest', () => {
           expect.any(Function)
         );
 
-        // Assert that the response status and body are correct
-        expect(response.status).toBe(200);
-        expect(response.body).toEqual({ data: "response body" });
+        // Assert that the error is handled properly
+        expect(error).toEqual(new Error("Request error"));
       });
     });
 });
