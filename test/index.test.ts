ca098e2659e0705b7a0faf295c1ad1d12cf4e16d
Jaynath Ray <126139404+jaynath-d@users.noreply.github.com>
2023-08-23 20:42:11 +0530

    Update index.test.ts

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,38 @@
-import { add } from '../src/index';
+import NodeApiRequest, { ApiResponse } from '../src/index';
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(add(1, 2)).toBe(3);
+describe('NodeApiRequest', () => {
+
+    // Tests that the sendRequest method successfully sends a GET request
+    it('should successfully send a GET request', () => {
+      // Mock the request function
+      const mockRequest = jest.fn((options, callback) => {
+        callback(null, { statusCode: 200 }, { data: "response body" });
+      });
+
+      // Replace the original request function with the mock
+      jest.mock("request", () => mockRequest);
+
+      // Call the sendRequest method with GET options
+      const options: RequestOptions = {
+        url: "http://example.com",
+        method: "GET",
+      };
+      return NodeApiRequest.sendRequest(options).then((response) => {
+        // Assert that the mock request function was called with the correct options
+        expect(mockRequest).toHaveBeenCalledWith(
+          {
+            url: "http://example.com",
+            method: "GET",
+            headers: undefined,
+            body: undefined,
+            json: true,
+          },
+          expect.any(Function)
+        );
+
+        // Assert that the response status and body are correct
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual({ data: "response body" });
+      });
+    });
 });
